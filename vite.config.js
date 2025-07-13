7ceecade142909252d8a860519b7e63a6de5c4a0
Aditya30-06 <aditya123adf@gmail.com>
2025-07-13 19:42:58 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/To-Do-React/', // 👈 VERY IMPORTANT
+  base: "/To-Do-React/", 
 })
